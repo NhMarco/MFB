3f031581705665c83b4b965e8da43b00e0803f60
Dreamleak <68868365+NhMarco@users.noreply.github.com>
2026-08-05 18:54:03 +0200

    Add files via upload

---
+++ Files/APPS/1237320.lua
@@ -0,0 +1,41 @@
+-- 1237320's Lua and Manifest Created by Hubcap Manifest
+-- Sonic Frontiers
+-- Created: September 30, 2025 at 18:20:46 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 8
+-- Total DLCs: 6
+-- Shared Depots: 2
+
+-- MAIN APPLICATION
+addappid(1237320) -- Sonic Frontiers
+-- MAIN APP DEPOTS
+addappid(1237321, 1, "9c57e01f36d7bf7a77b07678b08a0e690d785dfbcd9a421ae1fc2157741b5ccd") -- Depot 1237321
+setManifestid(1237321, "2773324310090316887", 41507797579)
+-- SHARED DEPOTS (from other apps)
+addappid(228988, 1, "1845444d5e2cfd0ae65ae4a8fedb6e2fbf776fcc5b913ab4ac461bc9a74f8358") -- VC 2019 Redist (Shared from App 228980)
+setManifestid(228988, "6645201662696499616", 29212173)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") -- DirectX Jun 2010 Redist (Shared from App 228980)
+setManifestid(228990, "1829726630299308803", 102931551)
+-- DLCS WITH DEDICATED DEPOTS
+-- Sonic Frontiers Explorers Treasure Box (AppID: 2079301)
+addappid(2079301)
+addappid(2079301, 1, "db37953f414051736b6e659e9146fa2e081148a94633fbea635610f79d6588da") -- Sonic Frontiers Explorers Treasure Box - Depot 2079301
+setManifestid(2079301, "3849979216096899603", 14478128)
+-- Sonic Frontiers Monster Hunter Collaboration Pack (AppID: 2089730)
+addappid(2089730)
+addappid(2089730, 1, "dfb354863019c75a8143f9e7073ed589df096057283247fe5e16c1c6225667f2") -- Sonic Frontiers Monster Hunter Collaboration Pack - Depot 2089730
+setManifestid(2089730, "3889377523269958120", 16627376)
+-- Sonic Frontiers Holiday Cheer Suit (AppID: 2089733)
+addappid(2089733)
+addappid(2089733, 1, "3dedee464853a2702ce024319deb87d57c6e2e404a328bce05f71b18a2649faf") -- Sonic Frontiers Holiday Cheer Suit - Depot 2089733
+setManifestid(2089733, "5603185428396252058", 2433520)
+-- Sonic Frontiers Sonic Adventure 2 Shoes (AppID: 2089840)
+addappid(2089840)
+addappid(2089840, 1, "cf0ee3bde87a208b2bf9425b695b287a2997de71728df89f826fca1667197f85") -- Sonic Frontiers Sonic Adventure 2 Shoes - Depot 2089840
+setManifestid(2089840, "71486794841630932", 4363952)
+-- Sonic Frontiers Digital Art Book with Mini Digital Soundtrack (AppID: 2093240)
+addappid(2093240)
+addappid(2093241, 1, "9eac17d35c7a4d92c2bc39bbfcd5a64c61df1d4aed6c5dc7bad49f962d4780bf") -- Sonic Frontiers Digital Art Book with Mini Digital Soundtrack - Depot 2093241
+setManifestid(2093241, "5565202849344929598", 1118687516)
+-- DLCS WITHOUT DEDICATED DEPOTS
+addappid(2079300) -- Sonic Frontiers Adventurers Treasure Box
---
+++ Files/APPS/648350.lua
@@ -0,0 +1,29 @@
+-- 648350's Lua and Manifest Created by Hubcap Manifest
+-- Jurassic World Evolution
+-- Created: September 29, 2025 at 20:19:24 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 3
+-- Total DLCs: 10
+-- Shared Depots: 2
+
+-- MAIN APPLICATION
+addappid(648350) -- Jurassic World Evolution
+-- MAIN APP DEPOTS
+addappid(648351, 1, "4fd37bc5e006aad39f9082a111b9e6ab7d94dd90024a7ac60f2b96d05327f8ec") -- Nero Content
+setManifestid(648351, "7860490337686912187", 7896812462)
+-- SHARED DEPOTS (from other apps)
+addappid(228986, 1, "51dd3611d28621644730736f3bb1fd6b960053a45cd79123f2b9a80c9181dad5") -- VC 2015 Redist (Shared from App 228980)
+setManifestid(228986, "8782296191957114623", 29759921)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") -- DirectX Jun 2010 Redist (Shared from App 228980)
+setManifestid(228990, "1829726630299308803", 102931551)
+-- DLCS WITHOUT DEDICATED DEPOTS
+addappid(817550) -- Jurassic World Evolution - Deluxe DLC
+addappid(817560) -- Jurassic World Evolution - Pre-order Content
+addappid(953940) -- Jurassic World Evolution Secrets of Dr Wu
+addappid(965420) -- Jurassic World Evolution - JingleJam
+addappid(979930) -- Jurassic World Evolution Cretaceous Dinosaur Pack
+addappid(988470) -- Jurassic World Evolution Carnivore Dinosaur Pack
+addappid(1028430) -- Jurassic World Evolution Claires Sanctuary
+addappid(1107760) -- Jurassic World Evolution Herbivore Dinosaur Pack
+addappid(1144410) -- Jurassic World Evolution Return To Jurassic Park
+addappid(1144420) -- Jurassic World Evolution Raptor Squad Skin Collection
